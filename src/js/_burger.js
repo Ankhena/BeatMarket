d65d63f30d034c9da7a1816a934850b32ef9d750
Alim <alim-halilko@mail.ru>
2021-05-08 15:32:31 +0300

    Страницы (создание портфеля +++) и 404 +++

--- src/js/_burger.js
+++ src/js/_burger.js
@@ -3,26 +3,9 @@ function initBurgerMenu() {
     let menu_burger = $("[data-burger='menu']");
     let overlay = document.querySelector(".overlay_burger");
     let body = document.body;
-
-    function hideMenu() {
-        menu_burger.slideUp('normal');
-        overlay.classList.remove("visible");
-        body.classList.remove("hideScroll");
-    }
-
-    function showMenu() {
-        menu_burger.slideDown('normal');
-        overlay.classList.add("visible");
-        body.classList.add("hideScroll");
-    }
     
     $(button_burger).click(() => {
-        if (!menu_burger.is(':visible')) {
-            showMenu()
-        } 
-        else {
-            hideMenu()
-        }
+        menu_burger.toggleClass("active")
     });
 
     overlay.addEventListener("click", () => {
