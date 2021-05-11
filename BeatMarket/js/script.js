db2479aff4b814474f9b245ebbe6b02dc838291d
Alim <alim-halilko@mail.ru>
2021-05-11 14:58:52 +0300

    дивиденды+++, верх. навигация+++, просм.профиля+++

--- BeatMarket/js/script.js
+++ BeatMarket/js/script.js
@@ -235,41 +235,54 @@ initSearchInput()
 }
 
 initMoveStrategy();
+    const myGraph = document.querySelector("#dividendsGraph");
+if (myGraph !== null) {
     Highcharts.chart('dividendsGraph', {
-    chart: {
-      type: 'variablepie',
-      backgroundColor: 'transparent'
-    },
-    title: {
-      text: ''
-    },
-    tooltip: {
-      enabled: false
-    },
-    series: [{
-      innerSize: '60%',
-      zMin: 1,
-      name: 'countries',
-      data: [{
-        name: 'AMAT',
-        y: 1000,
-        z: 100,
-        color: "red"
-      }, {
-        name: 'AMGN',
-        y: 1000,
-        z: 100
-      }, {
-        name: 'BFB',
-        y: 1000,
-        z: 100
-      }, {
-        name: 'Other',
-        y: 2000,
-        z: 100
-      }]
-    }]
-  });
+        chart: {
+            type: 'variablepie',
+            backgroundColor: 'transparent'
+        },
+        title: {
+            text: ''
+        },
+        tooltip: {
+            headerFormat: '',
+            pointFormat: `
+        <div style="text-align: center;">
+          <h5>{point.name}</h5>
+          <span>{point.y}$</span>
+        </div>
+      `,
+            useHTML: true
+        },
+        series: [{
+            innerSize: '60%',
+            zMin: 1,
+            name: 'countries',
+            data: [{
+                name: 'AMAT',
+                y: 1000,
+                z: 100,
+                color: "#199F27"
+            }, {
+                name: 'AMGN',
+                y: 1000,
+                z: 100,
+                color: "#DE4355"
+            }, {
+                name: 'BFB',
+                y: 1000,
+                z: 100,
+                color: "#3E54D8"
+            }, {
+                name: 'Other',
+                y: 2000,
+                z: 100,
+                color: "#929398"
+            }]
+        }]
+    });
+}
 
     document.querySelectorAll(".myStrategy-news-container .table-content-item").forEach(item => {
         item.addEventListener("click", (e) => {
--- src/js/_dividendsGraph.js
+++ src/js/_dividendsGraph.js
@@ -1,35 +1,48 @@
-Highcharts.chart('dividendsGraph', {
-    chart: {
-      type: 'variablepie',
-      backgroundColor: 'transparent'
-    },
-    title: {
-      text: ''
-    },
-    tooltip: {
-      enabled: false
-    },
-    series: [{
-      innerSize: '60%',
-      zMin: 1,
-      name: 'countries',
-      data: [{
-        name: 'AMAT',
-        y: 1000,
-        z: 100,
-        color: "red"
-      }, {
-        name: 'AMGN',
-        y: 1000,
-        z: 100
-      }, {
-        name: 'BFB',
-        y: 1000,
-        z: 100
-      }, {
-        name: 'Other',
-        y: 2000,
-        z: 100
-      }]
-    }]
-  });
+const myGraph = document.querySelector("#dividendsGraph");
+if (myGraph !== null) {
+    Highcharts.chart('dividendsGraph', {
+        chart: {
+            type: 'variablepie',
+            backgroundColor: 'transparent'
+        },
+        title: {
+            text: ''
+        },
+        tooltip: {
+            headerFormat: '',
+            pointFormat: `
+        <div style="text-align: center;">
+          <h5>{point.name}</h5>
+          <span>{point.y}$</span>
+        </div>
+      `,
+            useHTML: true
+        },
+        series: [{
+            innerSize: '60%',
+            zMin: 1,
+            name: 'countries',
+            data: [{
+                name: 'AMAT',
+                y: 1000,
+                z: 100,
+                color: "#199F27"
+            }, {
+                name: 'AMGN',
+                y: 1000,
+                z: 100,
+                color: "#DE4355"
+            }, {
+                name: 'BFB',
+                y: 1000,
+                z: 100,
+                color: "#3E54D8"
+            }, {
+                name: 'Other',
+                y: 2000,
+                z: 100,
+                color: "#929398"
+            }]
+        }]
+    });
+}
