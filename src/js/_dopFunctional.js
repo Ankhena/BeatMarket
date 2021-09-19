0d49029fe6615b436dc74c8efcbd80b308a13be4
Alim <alim-halilko@mail.ru>
2021-09-19 16:32:52 +0300

    добавлен блок прогноза в главном графике

--- src/js/_dopFunctional.js
+++ src/js/_dopFunctional.js
@@ -24,6 +24,7 @@ function dopFunctional() {
     let mainGraphSwitcherHeader = document.querySelectorAll(".mainGraphTypeChoose__item--header");
     let mainGraphSwitcherItems = document.querySelectorAll(".mainGraphTypeChoose__content .mainGraphTypeChoose__item");
     let mainGraphContent = document.querySelectorAll(".promoGraphArea__graphContent");
+    let forecastSwitcher = document.querySelectorAll(".promoGraphArea__selector.forecastGraph");
 
     mainGraphSwitcherItems.forEach(item => {
         item.addEventListener("click", function() {
@@ -33,6 +34,26 @@ function dopFunctional() {
                 elem.classList.toggle("mainGraphTypeChoose__item--active"); // переключаем активный класс
                 mainGraphContent[i].classList.toggle("promoGraphArea__graphContent--disabled");
             });
+
+            forecastSwitcher.forEach((forecast) => { // если есть переключатель прогноза, переключаем
+                forecast.classList.toggle("forecastGraph--disabled");
+            });
+
+        });
+    });
+
+    /////////// ПЕРЕКЛЮЧАТЕЛЬ ПРОГНОЗА ПРОМО ГРАФИК /////////////////////////
+
+    let forecastPromoContainer = $(".promoGraphArea__forecast");
+    let forecastClose = forecastPromoContainer.find(".forecastArea__close")
+
+    forecastSwitcher.forEach(elem => {
+        elem.querySelector(".forecastGraph__content").addEventListener("click", () => {
+            forecastPromoContainer.slideToggle();
+        });
+
+        forecastClose.click(() => {
+            forecastPromoContainer.slideToggle();
         });
     });
 }
